28c6632b0e3c6f4ccc867177d8ccb998c75622c3
Rplus <rplus.tw@gmail.com>
2015-02-28 17:53:43 +0800

    code-fiddle: simple pixk color

--- app/script/all.js
+++ app/script/all.js
@@ -1,7 +1,44 @@
 (function ($, window, document, undefined) {
 
-    $(function() {
-        // something to do after document ready
+  $(function() {
+    var _box = $('.box'),
+        _boxSize = _box.width(),
+        _mask = $('.mask'),
+        _dot = $('.dot'),
+        _color = {
+          'h': 0,
+          's': 0,
+          'l': '50%'
+        };
+
+    var calcColor = function (e) {
+      var _x = e.offsetX,
+         _y = e.offsetY;
+
+      _dot.css({
+        'left': _x,
+        'top': _y
+      });
+
+      _color.h = Math.round(_x * 255 / _boxSize);
+      _color.s = 100 - Math.round(_y * 100 / _boxSize);
+
+      _box.css({
+        'backgroundColor': 'hsl('+ _color.h +','+ _color.s +'%,50%)'
+      });
+    };
+
+    _mask.on({
+      'mouseenter.cc': function() {
+        _mask.on({
+          'mousemove.cc': calcColor
+        });
+      },
+      'click.cc, mouseleave': function () {
+        console.log('ccc');
+        _mask.off('mousemove.cc');
+      }
     });
+  });
 
 })(jQuery, window, document);
