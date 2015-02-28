66849f6901562e4ae8f4b5664884e10b68b86c5c
Rplus <rplus.tw@gmail.com>
2015-02-28 18:04:51 +0800

    remove png optimization task from master

--- gulpfile.js
+++ gulpfile.js
@@ -68,21 +68,6 @@ gulp.task('js', function () {
 
 
 
-gulp.task('png-min', ['png-clone-no-compress'],function () {
-  return gulp.src([path.sourceImage + '**/*.png', '!' + path.sourceImage + '**/*-no-compress.png'])
-    .pipe($.optipng(['-o2', '-strip all']))
-    .pipe(gulp.dest(path.buildImage));
-});
-
-
-
-gulp.task('png-clone-no-compress', function () {
-  return gulp.src([path.sourceImage + '**/*-no-compress.png'])
-    .pipe(gulp.dest(path.buildImage));
-});
-
-
-
 gulp.task('iconfont', function () {
   return gulp.src(path.sourceFont + 'svg/*.svg')
     .pipe($.iconfont({
@@ -120,7 +105,7 @@ gulp.task('watch', ['build'], function () {
   gulp.watch(path.sourceStyle + '**/*.styl', ['css']);
 });
 
-gulp.task('deploy', ['build', 'png-min'], function () {
+gulp.task('deploy', ['build'], function () {
   gulp.src(path.build + '**/*')
     .pipe($.ghPages());
 });
